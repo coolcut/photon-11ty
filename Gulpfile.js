ef5a8f5d5611747f12a71738bdcee3e4b11ce641
Jan Früchtl <jan@fruechtl.me>
2020-03-29 17:17:55 +0200

    Add gulp-responsive and try basic new image resizing tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const cssnano = require('gulp-cssnano');
+const responsive = require('gulp-responsive');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function() {
@@ -11,6 +12,47 @@ gulp.task('css', function() {
     .on('error', sass.logError)
     .pipe(gulp.dest('./_includes/css'));
 });
+
+gulp.task('images', function () {
+  return gulp.src('uploads/**/*.{png,jpg}')
+    .pipe(responsive({
+      '**/*': [{
+        width: 500,
+        rename: { suffix: '-500px' },
+      },{
+        width: 800,
+        rename: { suffix: '-800px' },
+      },{
+        width: 1200,
+        rename: { suffix: '-1200px' },
+      },{
+        width: 1500
+      },{
+        width: 2000,
+        rename: { suffix: '-2000px' },
+      },{
+        height: 500,
+        width: 500,
+        rename: { suffix: '-cover-500' },
+      },{
+        height: 800,
+        width: 800,
+        rename: { suffix: '-cover-800' },
+      },{
+        height: 1200,
+        width: 1200,
+        rename: { suffix: '-cover-1200' },
+      }]
+    },{
+      quality: 80,
+      progressive: true,
+      withMetadata: false,
+      withoutEnlargement: true,
+      errorOnUnusedImage: false,
+      errorOnEnlargement: false
+    }))
+    .pipe(gulp.dest('images'))
+})
 
 gulp.task("watch", function() {
   gulp.watch('./assets/scss/**/*.scss', gulp.parallel('css'));
