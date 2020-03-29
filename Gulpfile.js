52bfec0c13467d0d87c2711c35ee390bd9be382f
Jan Früchtl <jan@fruechtl.me>
2020-03-29 13:27:55 +0200

    Add Gulp with some basic scss tasks

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function() {
+  return gulp.src('./assets/scss/main.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./_includes/css'));
+});
+
+gulp.task("watch", function() {
+  gulp.watch('./assets/scss/**/*.scss', gulp.parallel('css'));
+});
+
+gulp.task('dev', gulp.parallel('css'));
